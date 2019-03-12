edfada05e9354966f582bb55012999df0d0a989d
To_In <mdmobshshir@gmail.com>
2019-03-12 19:42:29 +0530

    UI created
    simple layout with TextView ,2 Buttons(Start and Stop).

--- app/src/main/java/com/example/periodic_location_update/MainActivity.java
+++ app/src/main/java/com/example/periodic_location_update/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.periodic_location_update;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button start, stop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
