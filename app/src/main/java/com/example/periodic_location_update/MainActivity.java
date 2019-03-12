360515f52a801ef13a8018530bfaf0be1f0c0054
To_In <mdmobshshir@gmail.com>
2019-03-12 22:04:52 +0530

    Location Permission
    Permission Manaaged, asking permission when pressed start button. Used EasyPermission by Google.

--- app/src/main/java/com/example/periodic_location_update/MainActivity.java
+++ app/src/main/java/com/example/periodic_location_update/MainActivity.java
@@ -1,20 +1,73 @@
 package com.example.periodic_location_update;
 
+import android.Manifest;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
 
+import pub.devrel.easypermissions.AppSettingsDialog;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
 
     private Button start, stop;
+    private String[] perms = {Manifest.permission.ACCESS_FINE_LOCATION};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        start = findViewById(R.id.start);
+        stop = findViewById(R.id.stop);
 
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Start", Toast.LENGTH_SHORT).show();
+                requestLocationPermission();
+            }
+        });
 
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Stop", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void requestLocationPermission() {
+        if (EasyPermissions.hasPermissions(this, perms)) {
+            Toast.makeText(this, "Location Permission Granted", Toast.LENGTH_SHORT).show();
+        } else {
+            EasyPermissions.requestPermissions(this, "We need permissions For Location Services.",
+                    123, perms);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+        Toast.makeText(this, "Thanks For Permissions", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> list) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, list)) {
+            new AppSettingsDialog.Builder(this).build().show();
+        }
+        requestLocationPermission();
     }
 }
